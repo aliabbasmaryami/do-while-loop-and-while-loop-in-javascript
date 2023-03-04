aef9d2593722fc4cae5c2861983e1fc44851f8b8
Ali abbas <94835254+aliabbasmaryami@users.noreply.github.com>
2023-03-04 16:26:08 +0500

    Add files via upload

---
+++ practice.java/js/script.js
@@ -0,0 +1,28 @@
+
+// whhile loop synatx
+
+// while (condition) {
+    // code
+    // so-called "loop body"
+//   }
+
+let a= prompt("Enter value of n")
+n = Number.parseInt(a)
+let  b = 0;
+while(i<a){
+console.log(b)
+    i++
+}
+
+// do while loop in javascript
+// do {
+    // loop body
+//   } while (condition)
+
+let n = prompt("Enter value of n")
+n = Number.parseInt(n)
+let  i = 0;
+do{
+console.log(i)
+    i++
+}while(i<n)
